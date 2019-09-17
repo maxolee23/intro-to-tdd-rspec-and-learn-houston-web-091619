7fb102e30ea0dd7c9d7eb38905a1e56e594e5174
Max Lee <maxolee23@gmail.com>
2019-09-17 02:16:48 +0000

    Done.

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -7,3 +7,8 @@ describe "current_age_for_birth_year method" do
     expect(age_of_person).to eq(19)
   end
 end
+
+def current_age_for_birth_year(year)
+  age_of_person = 19
+  return age_of_person
+end
